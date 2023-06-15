102c1f32de6459d1b8bdc3a415ab071a1e8abb0d
arkhon7 <reviuy9@gmail.com>
2023-06-15 23:18:35 +0800

    added simple test

--- tests/test_parser.rs
+++ tests/test_parser.rs
@@ -1,13 +1,14 @@
-use mcl::parser;
-use mcl::tokenizer;
+use mcl::{Parser, Tokenizer};
 
 #[test]
 fn test_mcl() {
-    let file = std::fs::read("./sample.mcl").unwrap();
+    let file = std::fs::read("./tests/sample.mcl").unwrap();
 
-    let mut tokenizer = tokenizer::Tokenizer::new();
+    let mut tokenizer = Tokenizer::new();
     let tokens = tokenizer.tokenize(&file).unwrap();
 
-    let mut parser = parser::Parser::new();
-    parser.parse(&tokens).unwrap();
+    let mut parser = Parser::new();
+    let output = parser.parse(&tokens).unwrap();
+
+    println!("{:?}", output);
 }
