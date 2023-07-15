69210aa0f8dc455dbeb08676b6f50bd5694dd577
arkhon7 <reviuy9@gmail.com>
2023-07-15 18:31:49 +0800

    updated tests

--- tests/test_parser.rs
+++ tests/test_parser.rs
@@ -1,14 +1,37 @@
-use mcl::{Lexer, Parser};
+use serde_json::Value;
 
 #[test]
-fn test_mcl() {
+fn test_from_slice() {
     let file = std::fs::read("./tests/sample.mcl").unwrap();
 
-    let mut lexer = Lexer::new();
-    let tokens = lexer.tokenize(&file).unwrap();
+    let output = mcl::from_slice(&file).unwrap();
 
-    let mut parser = Parser::new();
-    let output = parser.parse(&tokens).unwrap();
+    assert!(output["jmmaa"] != Value::Null);
+}
 
-    println!("{:?}", output);
+#[test]
+fn test_from_str() {
+    let output = mcl::from_str(r#"foo { bar "baz" }"#).unwrap();
+
+    let num = &output["foo"]["bar"];
+
+    assert!(num.is_string());
+
+    let val = num.as_str();
+
+    assert!(val == Some("baz"))
+}
+
+#[test]
+fn test_arr() {
+    let output = mcl::from_str("\"marky\" 32 23.23 null").unwrap();
+
+    assert!(&output[0].is_string());
+    assert!(&output[1].is_number());
+    assert!(&output[2].is_number());
+    assert!(&output[3].is_null());
+
+    let val = output[2].as_f64();
+
+    assert!(val == Some(23.23));
 }
